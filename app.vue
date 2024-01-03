5d871cdee8775b6c677fe5df5d8c4136d7dd3c19
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-01-03 19:58:53 +0500

    basic setup & dashboard page template

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <NuxtPage/>
+    </div>
+</template>
+
+<script setup>
+
+useHead({
+    link: [
+        /* Google fonts */
+        {href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap', rel: 'stylesheet'},
+        /* Bootstrap icons */
+        {href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css', rel: 'stylesheet'},
+    ],
+    script: [
+        {src: '_nuxt/lib/jquery-3.7.1.min.js'},
+        {src: '_nuxt/lib/nicescroll.js'},
+        {src: '_nuxt/lib/charts/apex/apexcharts.min.js'},
+        {src: '_nuxt/assets/examples/dashboard.js'},
+        {src: '_nuxt/assets/app.js'},
+    ]
+})
+
+</script>
+
+<style src="./lib/slick/slick.css"></style>
+<style src="./assets/app.css"></style>
